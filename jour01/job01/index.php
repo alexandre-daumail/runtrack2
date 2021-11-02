3e0549da077f2a151473a890e4b0f37476fee89f
alexandre-daumail <alexandre.daumail@laplateforme.io>
2021-11-02 10:46:48 +0100

    Publication Job 01
    Création de trois variables et utilisation de la structure de langage php "echo"

---
+++ jour01/job01/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $str = "LaPlateforme"; // créez une variable nommée str. Affectez lui la valeur “LaPlateforme”
+    $str2 = "Vive";
+    $str3 = "!";
+  
+    echo "$str" . "<br>" . "$str2 $str $str3"; // Afficher la valeur str puis "Vive LaPlateforme !"
+
+?>
+
+
+
+    
