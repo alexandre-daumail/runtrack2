2b844935c3ae78318fd2f039f157bef9501665af
alexandre-daumail <alexandre.daumail@laplateforme.io>
2021-11-02 10:17:52 +0100

    Création index.php
    Hello la Plateforme!

---
+++ jour01/job00/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Index</title>
+        <meta charset="utf-8" />
+    </head>
+
+    <body>
+        
+        <?php echo "Hello LaPlateforme!"; ?>
+
+    </body>
+</html>
