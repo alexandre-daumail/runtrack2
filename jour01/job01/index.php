9d850664258c3632d91af19ac79b4ba0fa2952d9
alexandre-daumail <alexandre.daumail@laplateforme.io>
2021-11-02 14:14:20 +0100

    Ajout de variables job01
    Je pensais avoir fini le job
    => ajout des valeurs int et bool

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -6,6 +6,24 @@
   
     echo "$str" . "<br>" . "$str2 $str $str3"; // Afficher la valeur str puis "Vive LaPlateforme !"
 
+    /*Créez une variable nommée val. Affectez à cette variable la valeur 6. 
+Affichez le contenu de la variable. Ajoutez 4 à cette variable. Affichez 
+à nouveau le contenu.*/
+
+$val = 6;
+echo "<br>" . "$val";
+$val = 10;
+echo "<br>" . "$val";
+
+/*Créez une variable myBool, affectez à cette variable true, affichez le 
+contenu de la variable. Affectez false à la variable et affichez à 
+nouveau le contenu*/
+
+$myBool = "true";
+echo "<br>" . "$myBool";
+$myBool = "false";
+echo "<br>" . "$myBool";
+
 ?>
 
 
