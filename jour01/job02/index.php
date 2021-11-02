843107eb32d50f72fc62a7c25df9664817fc0c40
alexandre-daumail <alexandre.daumail@laplateforme.io>
2021-11-02 10:52:05 +0100

    Création Job02
    Utilisation de commentaires

---
+++ jour01/job02/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    $str = "Hello LaPlateforme!"; // créez une variable nommée str. Affectez lui la valeur “LaPlateforme”
+    $str2 = "Hello";
+    $str3 = "LaPlateforme!";
+  
+    echo $str; // Premier affichage "Hello laPlateforme!"
+    echo "<br>" . "$str2" . " $str3"; // Deuxième affichage "Hello laPlateforme!"
+?>
+
+
+
+    
