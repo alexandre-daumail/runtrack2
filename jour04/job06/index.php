9c004d9737aed319b53df7997e0837016906dca5
alexandre-daumail <alexandre.daumail@laplateforme.io>
2021-11-06 17:45:29 +0100

    Job 06 jour 4

---
+++ jour04/job06/index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Job 06 (Jour 4)</title>
+</head>
+
+<body>
+    <main>
+        <h1>Bienvenue sur mon site </h1>
+        <h2>Parité des nombres</h2>
+        <p>Si vous souhaitez enfin connaitre la vérité quant à la parité du nombre entier relatif que vous avez en tête, veuillez l'entrer dans la case "Nombre":</p>
+        <form name="inscription" method="post" action="#">
+            Nombre : <input type="number" name="nb" />
+            <input type="submit" name="valider" value="OK" /><br>
+        </form>
+    </main>
+    <aside>
+        <?php
+        if (isset($_POST['valider'])) {
+            if($_POST['nb'] % 2 ==0) {
+                echo 'Le nombre que vous avez entré est PAIR.';
+            }
+            else{
+                echo'Le nombre que vous avez entré est IMPAIR.';
+            }
+        }
+        ?>
+    </aside>
+</body>
+
+</html>
