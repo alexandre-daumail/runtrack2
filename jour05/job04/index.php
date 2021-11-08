d85336e695f04b6969836e7bd00dc4a9ff7a56a6
alexandre-daumail <alexandre.daumail@laplateforme.io>
2021-11-08 13:03:02 +0100

    job04 jour5

---
+++ jour05/job04/index.php
@@ -0,0 +1,29 @@
+<!-- Créez une fonction nommée “calcule” qui prend 3 paramètres : le premier, 
+“a”, est un nombre, le deuxième, “operation”, est un caractère contenant le 
+type d’opération (+, -, *, /, %), le troisième, “b”, est un nombre. La fonction 
+doit retourner le résultat de l’opération. -->
+
+<?php
+
+function calcule($a,$operation,$b){
+    if($operation == '+'){
+        $resultat = $a + $b;
+    }
+    if($operation == '-'){
+        $resultat = $a - $b;
+    }
+    if($operation == '*'){
+        $resultat = $a * $b;
+    }
+    if($operation == '/'){
+        $resultat = $a / $b;
+    }
+    if($operation == '%'){
+        $resultat = $a % $b;
+    }
+    return $resultat;
+}
+
+echo calcule(5, '+' , 2);
+
+?>
