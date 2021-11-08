17564cd69f197bec856812f16b3d10aaa552cfda
alexandre-daumail <alexandre.daumail@laplateforme.io>
2021-11-08 15:34:52 +0100

    job06 jour 05

---
+++ jour05/job06/index.php
@@ -0,0 +1,28 @@
+<!-- 
+    Créez une fonction nommée “leetspeak”. Cette fonction prend en 
+paramètre une chaîne de caractères nommée “str”. Elle doit retourner la 
+chaine de caractères “str” convertie en leet speak. Cela signifie qu’elle doit 
+la modifier de sorte à ce que : les “A” deviennent des “4”, les “B” des “8”, 
+les “E” des “3”, les “G” des “6”, les “L” des “1”, les “S” des “5” et les “T” des 
+“7”. Cela est valable que l’on crie ou non (majuscules et minuscules).
+-->
+
+<?php
+
+function leetspeak($str){
+    $leet = ['a' => '4', 'A' =>'4', 'b'=>'8', 'B'=>'8', 'E'=>'3', 'e'=>'3', 'G'=>'6', 'g'=>'6', 'L'=>'1', 'l'=>'1', 'S'=>'5', 's'=>'5', 'T'=>'7', 't'=>'7'];
+
+        for($x=0; isset($str[$x])==true; $x++){
+            foreach($leet as $cle => $valeur){
+                if ($str[$x] == $cle){
+                    $str[$x] = $valeur;
+                }
+            }
+        }
+        return $str;    
+}
+
+
+echo leetspeak('aaaaaaa');
+?>
+
