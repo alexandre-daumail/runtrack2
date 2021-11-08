48d6e832030d3703d0f4257b9522033c37f12de0
alexandre-daumail <alexandre.daumail@laplateforme.io>
2021-11-08 11:19:33 +0100

    Job01 Jour5

---
+++ jour05/job01/index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+</head>
+
+<?php
+function hello(){
+        echo 'Hello LaPlateforme!';
+    }
+
+?>
+
+
+<body>
+<?php
+
+
+hello();
+
+?>
+
+</body>
+
+</html>
