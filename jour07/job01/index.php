6d41b3b3d7fdeffb6447bcb7f641198603fd1cfe
alexandre-daumail <alexandre.daumail@laplateforme.io>
2021-11-09 11:39:18 +0100

    création bouton reset job 01/j07

---
+++ jour07/job01/index.php
@@ -0,0 +1,42 @@
+<!-- Créez une variable de session nommée “nbvisites”. A chaque fois que la
+page est visitée, ajoutez 1. Afficher le contenu de cette variable.
+Ajoutez un bouton nommé “reset” qui permet de réinitialiser ce compteur. -->
+
+<?php
+session_start(); //initialisation de la session
+
+
+if(isset($_POST["reset"])){
+    unset($_SESSION["nbvisites"]);
+    
+}
+
+if (isset($_SESSION["nbvisites"]) == true) { // création de boucle de comptage. la premiere fois que la page est chargée,
+    //le isset sera false donc on commencera par initialiser le comptage au niveau du "else" plus bas.
+    $_SESSION["nbvisites"]++; // une fois que la page a été chargée une premiere fois, on incrémentera la variable 
+} else {
+    $_SESSION["nbvisites"] = 0;
+}
+echo $_SESSION["nbvisites"];
+
+
+?>
+
+<!doctype html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+
+<body>
+    <form action="#" method="post">
+        <input type="submit" name="reset" value="Reset">
+
+    </form>
+</body>
+
+</html>
