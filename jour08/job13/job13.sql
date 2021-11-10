4a9547c3085a057136dd2b3b43d6cac835c34ab0
alexandre-daumail <alexandre.daumail@laplateforme.io>
2021-11-10 17:23:45 +0100

    jobs 11-12-13-14-15

---
+++ jour08/job13/job13.sql
@@ -0,0 +1,4 @@
+/* Ecrivez dans le fichier “job13.sql” une requête permettant de sélectionner 
+la capacite moyenne des salles. */
+
+SELECT AVG (capacite) FROM salles
---
+++ jour08/job15/job15.sql
@@ -0,0 +1,3 @@
+/* Ecrivez dans le fichier “job15.sql” une requête permettant de récupérer le 
+nom des salles et le nom de leur étage. */
+
