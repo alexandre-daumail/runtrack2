9d296ec9c72c8db9f597c6312e3e91edf6ad64eb
alexandre-daumail <alexandre.daumail@laplateforme.io>
2021-11-11 20:20:15 +0100

    Update job18.sql
    J'ai trouvé la fonction AS pour donner un alias à salles.nom

--- jour08/job18/job18.sql
+++ jour08/job18/job18.sql
@@ -1,1 +1,6 @@
-SELECT salles.nom, etage.nom, MAX(`capacite`) FROM salles LEFT JOIN etage ON salles.id_etage = etage.id ORDER BY etage.nom;
+/* Ecrivez dans le fichier “job18.sql” une requête permettant de récupérer le 
+nom de l'étage ayant la salle avec la plus grande capacité (et afficher aussi 
+le nom de cette salle ainsi que sa capacité). Dans ce résultat, la colonne 
+“nom” de la salle doit être renommée en “Biggest Room” */
+
+SELECT salles.nom AS 'Biggest_Room', etage.nom, MAX(`capacite`) FROM salles LEFT JOIN etage ON salles.id_etage = etage.id ORDER BY etage.nom;
